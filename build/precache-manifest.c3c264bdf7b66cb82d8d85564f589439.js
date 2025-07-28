95290800255bfc6cbd8952bd06f5c797fadbd08b
PriyanshiPrateek812 <priyanshiprateek812@gmail.com>
2025-07-28 19:27:36 +0530

    Fix homepage URL for GitHub Pages

---
+++ build/precache-manifest.c3c264bdf7b66cb82d8d85564f589439.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3623f7388f18c98f414a5b9691f9d584",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/index.html"
+  },
+  {
+    "revision": "4d79d53493cf228385cc",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/js/2.97505cae.chunk.js"
+  },
+  {
+    "revision": "edccbfa5ea44de860add",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/js/main.35c7eb5a.chunk.js"
+  },
+  {
+    "revision": "d0fd456d53d08996a6f7",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/js/runtime-main.105e67c1.js"
+  },
+  {
+    "revision": "885ee36711f16c9c5115789a61c2bb99",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/media/step1.885ee367.png"
+  },
+  {
+    "revision": "d113d23d1de32235ab8593fa290d9012",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/media/step2.d113d23d.png"
+  },
+  {
+    "revision": "5878cae9245832768ca60d4227aa25d6",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/media/step3.5878cae9.png"
+  },
+  {
+    "revision": "a321e615db779bf33565259421350b32",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/media/step4.a321e615.png"
+  },
+  {
+    "revision": "2756f0107e8206631ee0ddd2779a3831",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/media/step5.2756f010.png"
+  },
+  {
+    "revision": "2ee72f55f427c3d4389a704cc1691132",
+    "url": "/PriyanshiPrateek812/CoffeeWebsite/static/media/step6.2ee72f55.png"
+  }
+]);
